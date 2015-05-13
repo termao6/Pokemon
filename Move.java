1f3b8f21b4f5aa5d581008f63856d77bd1459d77
ahu7 <ahu7@students.d125.org>
2015-05-13 15:03:58 -0500

    Created trainer and move classes

--- Move.java
+++ Move.java
@@ -8,26 +8,29 @@
 public class Move
 {
     // instance variables - replace the example below with your own
-    private int x;
-
+    private String name;
+    private String type;
+    private int baseAttack;
+    
     /**
      * Constructor for objects of class Move
      */
-    public Move()
+    public Move(String n, String t, int ba) 
     {
-        // initialise instance variables
-        x = 0;
+        name = n;
+        type = t;
+        baseAttack = ba;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public int getBaseAttack() {
+        return baseAttack;
     }
 }
--- Trainer.java
+++ Trainer.java
@@ -5,29 +5,18 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.util.*;
 public class Trainer
 {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Trainer
-     */
-    public Trainer()
+    private List<Pokemon> list;
+    public Trainer(List<Pokemon> l)
     {
-        // initialise instance variables
-        x = 0;
+        list = l;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
+    public List<Pokemon> getList()
     {
-        // put your code here
-        return x + y;
+        return list;
     }
 }
