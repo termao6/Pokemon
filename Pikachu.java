668d181e1a1900a24e7f8514a445f961120bc902
Teresa <termao6@students.d125.org>
2015-05-13 15:22:59 -0500

    worked on Pikachu

--- Pikachu.java
+++ Pikachu.java
@@ -12,8 +12,23 @@ public class Pikachu
     private int level, EXP;
     private int HPFull, HPBattle, attack, defense;
     private List<Move> attacks = new ArrayList<Move>();
-    public Pikachu (){
-        //attacks.add(new Move("Thunder Shock", "Electric", 40));
-        //attacks.add(new Move("Growl", "Normal", 0));
+    public Pikachu (int lev){
+        attacks.add(new Move("Thunder Shock", "Electric", 40));
+        attacks.add(new Move("Growl", "Normal", 0));
+        level = lev;
+        attack = (int) (Math.random()*(level/4)) + 15;
+        defense = (int) (Math.random()*(level/5)) + 12;
+        HPFull = (int) (Math.random()*(level/3)) + 16;
+        HPBattle = HPFull;
+    }
+    public void gainEXP (int points){
+        EXP += points;
+        if (EXP/3 > level) {
+            level++;
+            attack += (int) (Math.random()*(level/4));
+            defense = (int) (Math.random()*(level/5));
+            HPFull = (int) (Math.random()*(level/3));
+            HPBattle = HPFull;
+        }
     }
 }
