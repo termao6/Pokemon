56416a0bf06de07cbd93eca9b8208567b5de4082
Teresa <termao6@students.d125.org>
2015-05-13 15:07:07 -0500

    Pikachu

---
+++ Pikachu.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Pikachu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+public class Pikachu
+{
+    private int level, EXP;
+    private int HPFull, HPBattle, attack, defense;
+    private List<Move> attacks = new ArrayList<Move>();
+    public Pikachu (){
+        //attacks.add(new Move("Thunder Shock", "Electric", 40));
+        //attacks.add(new Move("Growl", "Normal", 0));
+    }
+}
