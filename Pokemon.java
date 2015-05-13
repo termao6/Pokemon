7c962a9d3c23ef237a04a3d985ec5afb2382c41d
Teresa <termao6@students.d125.org>
2015-05-13 14:46:46 -0500

    Created Pokemon Interface

---
+++ Pokemon.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of interface Pokemon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Pokemon
+{
+    public void gainEXP();
+}
